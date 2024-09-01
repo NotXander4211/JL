bebf94d386656bec8b1998bdc341867ab32d554a
NotXander <xandercode2@gmail.com>
2024-09-01 14:24:58 -0700

    errs, tokens

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,33 @@
+class Error:
+    def __init__(self, name, details):
+        self.name = name
+        self.details = details
+    def toString(self):
+        return f"{self.name}: {self.details}"
+    
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super.__init__(self, "IllegalCharError", details)
+
+#TOKEN
 class Token:
-    def __init__(self):
-        pass
+    def __init__(self, _type, val):
+        self.type = _type
+        self.value = val
+    def __repr__(self):
+        if self.value:
+            return f"{self.type}:{self.value}"
+        return f"{self.type}:&NOVALUE&"
+    
+#constant types
+TT_INT = "INT"
+TT_FLOAT = "FLOAT"
+TT_PLUS = "PLUS"
+TT_MINUS = "MINUS"
+TT_MUL = "MUL"
+TT_DIV = "DIV"
+TT_LPAREN = "LPAREN"
+TT_RPAREN = "RPAREN"
+TT_EOF = "EOF"
+TT_RBRACKET = "RBRACKET"
+TT_LBRACKET = "LBRACKET"
