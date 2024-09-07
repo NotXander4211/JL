f9d2735e6efb79b7c5ee4b524cc5830b988d2afa
NotXander <xandercode2@gmail.com>
2024-09-07 00:33:16 -0700

    better type support and jump statements, python only

--- src/helper.py
+++ src/helper.py
@@ -1,16 +1,20 @@
 class TypeError(Exception):
     pass
 
+class MissingArgumentError(Exception):
+    pass
+
 class Stack:
     def __init__(self, size):
         self.buf = [0 for _ in range(size)]
-        self.pt = 0
+        self.pt = -1
     def push(self, value: int):
         self.pt += 1
         self.buf[self.pt] = value
     def pop(self):
-        number = self.buf[self.pt]
-        self.pt -= 0
+        #print(self.buf[self.pt])
+        number = int(self.buf[self.pt])
+        self.pt -= 1
         return number
     def top(self):
         return self.buf[self.pt]
@@ -24,3 +28,31 @@ def CheckType(type1, type2, wantedType1, wantedType2):
     if type(type2) == wantedType2:
         boolType2 = True
     return boolType1 and boolType2
+
+def JumpStatement(statement, top):
+    res = False
+    args = statement.split(".")
+    args[1] = args[1].lower()
+    # this is fine bc we only need args[2] for ints for now
+    if len(args) >= 3:
+        args[2] = int(args[2])
+    match args[1]:
+            case "eq":
+                if top == args[2]:
+                    res = True
+            case "gt":
+                if top >= args[2]:
+                    res = True
+            case "lt":
+                if top <= args[2]:
+                    res = True
+            case "tr":
+                #looks worse but needed bc will always evaluate to true if there is value, same for case "fa"
+                if top == True:
+                    res = True
+            case "fa":
+                if top == False:
+                    res = True
+            case default:
+                raise MissingArgumentError("Missing argument for jump statement")
+    return res
--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,6 @@
-from helper import Stack, CheckType
+from helper import Stack, CheckType, JumpStatement, TypeError, MissingArgumentError
 
+debug = False
 filen = "./src/test.jail"
 
 programL = []
@@ -18,25 +19,48 @@ for line in programL:
     if opcode.endswith(":"):
         lt[opcode[:-1]] = tc
         continue
+    if opcode.startswith("??"):
+        #this is a comment ^
+        continue
     
     program.append(opcode)
     tc += 1
     if opcode == "push":
         #should give variable for now its only numbers(int)
-        num = args[1]
-        program.append(num)
+        type_ = args[1]
+        var = args[2]
+        match type_.lower():
+            case "int":
+                program.append(int(var))
+            case "str":
+                program.append(str(var))
+            case "bool":
+                if var.lower() == "true":
+                    program.append(True)
+                else:
+                    program.append(False)
+            case "list":
+                var = var.split(",")
+                for i in var:
+                    var[i] = var[i].strip()
+                program.append(var)
         tc += 1
     if opcode == "print":
-        stringL = " ".join(args[1:])
+        if args[1].lower() == "top":
+            stringL = "__top__"
+        elif args[1].lower() == "stack":
+            stringL = "__stack__"
+        else:
+            stringL = " ".join(args[1:])[1:-1]
         program.append(stringL)
         tc += 1
-    if opcode == "jump.eq.0":
+    if opcode.startswith("jump"):
         label = args[1].lower()
         program.append(label)
         tc += 1
-        
-print(program)
-print(lt)
+if debug:
+    print(program)
+    print(lt)
     
 pc = 0
 stack = Stack(256)
@@ -45,15 +69,18 @@ while program[pc] != "halt":
     opcode = program[pc]
     pc += 1
     if opcode == "push":
-        print("printp")
         stack.push(program[pc])
         pc += 1
     elif opcode == "read":
         num = int(input(""))
         stack.push(num)
     elif opcode == "print":
-        print("print")
-        strL = program[pc]
+        if program[pc] == "__top__":
+            strL = "TOP: " + str(stack.top())
+        elif program[pc] == "__stack__":
+            strL = "TOP: " + str(stack.buf) + " : " + str(stack.pt)
+        else:
+            strL = program[pc]
         print(strL)
         pc += 1
     elif opcode == "add":
@@ -63,11 +90,10 @@ while program[pc] != "halt":
             stack.push(a + b)
         else:
             raise TypeError("Top 2 variables in the stack are both not Type int")
-    elif opcode == "jump.eq.0":
-        print("prinwet")
-        num = stack.top()
-        print(stack.top())
-        if num == 0:
+    elif opcode.startswith("jump"):
+        theTop = stack.top()
+        # print(stack.top())
+        if JumpStatement(opcode, theTop):
             pc = lt[program[pc]]
         else:
             pc += 1
